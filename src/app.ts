935a1c79e06807c5dbebabda9dc85a0a14807ff8
backspacev9 <backspacev9@gmail.com>
2021-07-14 20:52:52 +0300

    fix port

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ import categories from "./categoriesPage/router";
 const app = express();
 const PORT = process.env.PORT || 3030;
 app.use("/api/category", categories);
+app.get("/", (req, res) => {
+  res.end(`asdsadasd`);
+});
 
 app.listen(PORT, () => {
   console.log("server started");
