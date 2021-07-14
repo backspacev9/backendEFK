f495dbde059c644f392c7d85a83ebe0a856e4fe5
backspacev9 <backspacev9@gmail.com>
2021-07-15 00:46:47 +0300

    fix port2

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,8 @@ import express from "express";
 import categories from "./categoriesPage/router";
 
 const app = express();
-const PORT = process.env.PORT || 3030;
-//app.use("/api/category", categories);
+let PORT = process.env.PORT || 3000;
+app.use("/api/category", categories);
 app.get("/", (req, res) => {
   res.end(`asdsadasd`);
 });
